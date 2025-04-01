2986d7a3d118c93691e0c87f7440d09452fc47b4
ikerserna <146813295+ikerserna@users.noreply.github.com>
2025-04-01 21:25:03 +0200

    responsive login.php

--- php/login.php
+++ php/login.php
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <main>
         <div class="logo">
-            <img src="./../img/logo.png" alt="logo">
+            <img src="./../img/logo.png" alt="logo" class="logo-img">
         </div>
         <section class="login-form-container">
             <h1>Welcome Back!</h1>
